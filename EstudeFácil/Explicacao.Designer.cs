488d48f9ff0a973bec688480ebafb5ddca0f775d
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-06-04 14:16:56 -0300

    FirstCommit

---
+++ EstudeFácil/Explicacao.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace EstudeFácil
+{
+    partial class Explicacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Location = new System.Drawing.Point(636, 316);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(75, 23);
+            this.btnFinalizar.TabIndex = 0;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Agora explique o que você sabe sobre o assunto:";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(12, 35);
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(699, 265);
+            this.txtTexto.TabIndex = 2;
+            // 
+            // Explicacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 358);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFinalizar);
+            this.Name = "Explicacao";
+            this.Text = "Explicacao";
+            this.Load += new System.EventHandler(this.Explicacao_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTexto;
+    }
+}
